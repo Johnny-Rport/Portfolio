366b1793bcb1100f3e367dc273c2f97d32d4b8e1
NASA-DATABASE\Scoob <jrangel18419@gmail.com>
2020-11-10 08:31:03 -0800

    Changes: Fixed button issue, and so muiltple descriptions can now be hidden and revealed, and little tweaks to some pages.

--- Function/webfu.js
+++ Function/webfu.js
@@ -1,19 +1,19 @@
-const des0 = document.querySelector('[valueAsDes0]');
-/*const des1 = document.querySelector('[valueAsDes1]');
+const des = document.querySelector('[valueAsDes0]');
+const des1 = document.querySelector('[valueAsDes1]');
 const des2 = document.querySelector('[valueAsDes2]');
-const des3 = document.querySelector('[valueAsDes3]'); *///Original values for each of the seperate paragraphs, but arrays scared me at the time.
-const button = document.querySelector('[valueAsButton]');
+const des3 = document.querySelector('[valueAsDes3]');
 
 
-button.addEventListener('click', showy);
-//let des = [des0, des1, des2, des3,] //My array that I did not know how to implement at the time maybe I can return to this and overengineer this to oblivion.
+const showDes = [des, des1, des2, des3] //* I know I don't need this array anymore, but as an example on how to use arrays I'll keep it, and this is my website
+
 let buttonMode = 1;
-function showy () {
+function showy(x) {
         if (buttonMode === 1) {
-                des0.setAttribute('class', (''));
+                x.setAttribute('class', (''));
                 buttonMode = 0;
         } else if (buttonMode === 0) {
-                des0.setAttribute('class' , ('hide'));
+                x.setAttribute('class' , ('hide'));
                 buttonMode = 1;
         }
 }
+//Next try to get the banner to start moving depending on mouse position
