e6c89405f12cd2e218ca0658eaae0eb04ba59a9f
DESKTOP-3FG302D\darkb <Johnny Rangel>
2021-10-31 11:42:38 -0700

    adding the list

--- data_web/My_Projects/Project_List/resources/logic.js
+++ data_web/My_Projects/Project_List/resources/logic.js
@@ -3,41 +3,49 @@ const listButton = document.querySelector("[listButton]")
 const userInput = document.querySelector("[itemUser]")
 
 listButton.addEventListener('click', () => {
-    createItem()
-})
-
-// Fucking break apart this function too many responsibilities
-function createItem() {
     const newItem = document.createElement("div")
     const newDesc = document.createElement("p")
     const newDelete = document.createElement("button")
     const newEdit = document.createElement("button")
-
+    
     container.appendChild(newItem)
     newItem.classList.add('newItem')
+    
+    createItem(newItem, newDesc, userInput)
+    createDelete(newItem, newDelete)
+    createEdit(newItem, newDesc, newEdit)
+})
 
-    let textContent = document.createTextNode(userInput.value)
+function createItem(newItem, newDesc, newInput) {
+    let textContent = document.createTextNode(newInput.value)
     newItem.appendChild(newDesc)
     newDesc.appendChild(textContent)
     newDesc.classList.add("newDesc")
     
-    textContent = document.createTextNode("Delete")
+}  
+    
+    
+function createDelete(newItem, newDelete) {
+    let textContent = document.createTextNode("Delete")
     newItem.appendChild(newDelete)
     newDelete.appendChild(textContent)
     newDelete.classList.add("newButton")
+    
+    newDelete.addEventListener('click', ()=> {
+        deleteItem(newItem)
+    })
+}
 
-    textContent = document.createTextNode("Edit")
+function createEdit(newItem, newDesc, newEdit) {
+    let textContent = document.createTextNode("Edit")
     newItem.appendChild(newEdit)
     newEdit.appendChild(textContent)
     newEdit.classList.add("newButton")
 
-    newDelete.addEventListener('click', ()=> {
-        deleteItem(newItem)
-    })
-
     newEdit.addEventListener('click', ()=> {
         editItem(newItem, newDesc)
     })
+
 }
 
 function deleteItem(parentDiv) {
@@ -45,25 +53,27 @@ function deleteItem(parentDiv) {
 }
 
 function editItem(parentDiv, parentDesc) {
-
     const newInput = document.createElement("input")
     const newSubmit = document.createElement("button")
-    parentDiv.removeChild(parentDesc)
     
+    let textContent = parentDesc.innerText
     parentDiv.appendChild(newInput)
+    newInput.value = textContent
     newInput.classList.add("newThings")
     
+    textContent = document.createTextNode("Submit")
     parentDiv.appendChild(newSubmit)
     newSubmit.classList.add("newThings")
-    
-    let textContent = document.createTextNode("Create an Item")
     newSubmit.appendChild(textContent)
     
+    parentDesc.innerText = ''
+    parentDiv.removeChild(parentDesc)
     
     newSubmit.addEventListener('click', ()=> {
-        textContent = newInput.value
-        console.log(textContent)
-        // text is save, now save it into a new p element which will then delete the other ones since its no longer needed, ultimately, the edit is succesful
+        createItem(parentDiv, parentDesc, newInput)
+        parentDiv.removeChild(newInput)
+        parentDiv.removeChild(newSubmit)
+
     })
 }
 
