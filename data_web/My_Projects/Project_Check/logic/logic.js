51437cf37e62c8996278de9c5705f8e908a6b2f2
DESKTOP-3FG302D\darkb <Johnny Rangel>
2021-11-14 11:01:20 -0800

    Making changes to check calculator, I'm improving it to handle two sets of inputs only.

--- data_web/My_Projects/Project_Check/logic/logic.js
+++ data_web/My_Projects/Project_Check/logic/logic.js
@@ -1,50 +1,106 @@
+// Input Elements
 const inputWage = document.querySelector('[wage]')
-const hours = document.querySelector('[Hour]')
-const days = document.querySelector('[Day]')
+const inputHours = document.querySelector('[Hour]')
+const inputDays = document.querySelector('[Day]')
+
 const inputWage2 = document.querySelector('[wage2]')
 const hours2 = document.querySelector('[Hour2]')
 const days2 = document.querySelector('[Day2]')
 
-const reset = document.querySelector('[reset]')
-const submit = document.querySelector('[submit]')
-const reset2 = document.querySelector('[reset2]')
-const submit2 = document.querySelector('[submit2]')
-
+// Buttons
 const dailyp = document.querySelector('[daily]')
 const yearlyp = document.querySelector('[yearly]')
 const monthlyp = document.querySelector('[monthly]')
 const weeklyp = document.querySelector('[weekly]')
 const check = document.querySelector('[check]')
 
-const pay = [dailyp, yearlyp, monthlyp, weeklyp, check]
-const hiddenE = document.querySelectorAll('[hidden]')
-const shownE = document.querySelectorAll('[shown]')
-const outputP = document.querySelector('[result]')
+const switchButton = document.querySelector('[switch]')
+const reset = document.querySelector('[reset]')
+const submit = document.querySelector('[submit]')
 const compare = document.querySelector('[compare]')
 
+// Arrays, Outputs, and more
+const pay = [dailyp, yearlyp, monthlyp, weeklyp, check]
+const firstSet = document.querySelectorAll('[first]')
+const secondSet = document.querySelectorAll('[second]')
+const outputP = document.querySelector('[result]')
+let switchSets = 1
+
+
+
+// This function is essential in making the compare work
+// submit2.addEventListener('click', ()=> { 
+//     if (inputWage2.value != '' && hours2.value != '' && days2.value != '') {
+//         inputWage2.setAttribute('disabled', '')
+//         hours2.setAttribute('disabled', '')
+//         days2.setAttribute('disabled', '')
+
+
+//         pay.forEach(button => {
+//             button.removeAttribute('disabled')
+//             button.addEventListener('click', () => {
+//                 let change = true
+//                 calcPay(button.innerText[0], inputWage2.value, hours2.value, days2.value, change)
+//             })
+//         })
+//     }
+// })
+
+// Event Listeners
+switchButton.addEventListener('click', () => {
+    switchInputs(firstSet, secondSet)
+})
+
 reset.addEventListener('click', () => {
-    inputWage.value = ''
-    inputWage.removeAttribute('disabled')
+    resetValues(inputWage, inputHours, inputDays)
+})
+
+submit.addEventListener('click', ()=> {
+    submitValues(inputWage, inputHours, inputDays)
+})
+
+// Switches between first and second set of values
+// You might have to incorporate reset and submit event listeners in order to tell which set is selected
+function switchInputs (firstSet, secondSet) {
+    if (switchSets === 1) {
+        firstSet.forEach(element => {
+            element.setAttribute('disabled', '')
+        })
+
+        secondSet.forEach(element => {
+            element.removeAttribute('disabled')
+        })
+
+        switchSets = 0 
+        alert('Second Set of Values')
+
+    } else if (switchSets === 0) {
+        firstSet.forEach(element => {
+            element.removeAttribute('disabled')
+        })
+
+        secondSet.forEach(element => {
+            element.setAttribute('disabled', '')
+        })
+
+        switchSets = 1
+        alert('First Set of Values')
+    }
+
+}
+
+function resetValues (wage, hours, days) {
+    wage.value = ''
+    wage.removeAttribute('disabled')
     hours.value = ''
     hours.removeAttribute('disabled')
     days.value = ''
     days.removeAttribute('disabled')
-    
-})
+}
 
-reset2.addEventListener('click', () => {
-    inputWage2.value = ''
-    inputWage2.removeAttribute('disabled')
-    hours2.value = ''
-    hours2.removeAttribute('disabled')
-    days2.value = ''
-    days2.removeAttribute('disabled')
-})
-
-submit.addEventListener('click', ()=> {
-
-    if (inputWage.value != '' && hours.value != '' && days.value != '') {
-        inputWage.setAttribute('disabled', '')
+function submitValues (wage, hours, days) {
+    if (wage.value != '' && hours.value != '' && days.value != '') {
+        wage.setAttribute('disabled', '')
         hours.setAttribute('disabled', '')
         days.setAttribute('disabled', '')
         
@@ -55,38 +111,8 @@ submit.addEventListener('click', ()=> {
                 calcPay(button.innerText[0], inputWage.value, hours.value, days.value, change)
             })
         })
-    }
-})
-
-submit2.addEventListener('click', ()=> { //Instead of copying the first button this will instead show a another table that will show the comparision of all the salaries between the first and second values.
-
-    if (inputWage2.value != '' && hours2.value != '' && days2.value != '') {
-        inputWage2.setAttribute('disabled', '')
-        hours2.setAttribute('disabled', '')
-        days2.setAttribute('disabled', '')
-        
-        pay.forEach(button => {
-            button.removeAttribute('disabled')
-            button.addEventListener('click', () => {
-                let change = true
-                calcPay(button.innerText[0], inputWage2.value, hours2.value, days2.value, change)
-            })
-        })
-    }
-})
-
-compare.addEventListener('click', () => {
-    hiddenE.forEach(element => {
-        element.removeAttribute('hidden')
-    })
-
-    shownE.forEach(element => {
-        element.setAttribute('hidden', '')
-    })
-
-    alert('Enter the second set of values')
-})
-
+    }  
+}
 
 function calcPay (input, wage, hour, day, compare) {
     let week = 2 // In Weeks
