0ef5dedac386ef0e186ed5b1c8fec810f17e1527
DESKTOP-3FG302D\darkb <Johnny Rangel>
2021-12-27 19:25:38 -0800

    Updating List

--- data_web/My_Projects/Project_List/resources/logic.js
+++ data_web/My_Projects/Project_List/resources/logic.js
@@ -2,30 +2,62 @@ const container = document.querySelector("[container]")
 const listButton = document.querySelector("[listButton]")
 const userInput = document.querySelector("[itemUser]")
 const deleteButton = document.querySelector("[deleteDone]")
+const undoButton = document.querySelector('[undoButton]')
+let currentState = []
+let historyState = []
+let indexCount = -1
 
+setHistory(currentState)
 listButton.addEventListener('click', () => {
     const newItem = document.createElement("div")
     const newDesc = document.createElement("p")
     
     container.appendChild(newItem)
-    newItem.classList.add('newItem')
-
-    newDesc.addEventListener('click', ()=> {
-        if (newItem.hasAttribute('done') != true) {
-            markDone(newItem)
-        } else if (newItem.hasAttribute('done') == true) {
-            newItem.removeAttribute('done')
-        }
-    })
-
-    newItem.addEventListener('dblclick', ()=> {
-        editItem(newItem, newDesc)
-    })
+    newItem.setAttribute('newItem', '')
+    
    
     createItem(newItem, newDesc, userInput)
     createDelete(newItem, newDesc)
     createEdit(newItem, newDesc)
-    createDone(newItem)
+    createDone(newItem, newDesc)
+})
+
+deleteButton.addEventListener('click', ()=> {
+    let doneItems = document.querySelectorAll('[done]')
+    doneItems.forEach(item => {
+        container.removeChild(item)
+    })
+    
+})
+
+undoButton.addEventListener('click', ()=> {
+    undoButton.setAttribute('disabled', '')
+    let verify = indexCount >= 1
+    switch(verify) {
+        case true:
+            let mainChilds = document.querySelectorAll('[newItem]')
+            mainChilds.forEach(child => {
+                container.removeChild(child)
+            })
+            
+            let indexSelector = --indexCount
+            for (let state = 0; state < historyState.length; state++) {
+                if (indexSelector == state) {
+                    currentState = historyState[state]
+                    currentState.forEach(element => {
+                        container.appendChild(element)
+                    })
+                }
+            }
+            indexCount += 2
+            setHistory(true)
+        break;
+            
+        
+        default:
+            console.log('broken')
+        break;
+    }
 })
 
 function createItem(newItem, newDesc, newInput) {
@@ -33,7 +65,9 @@ function createItem(newItem, newDesc, newInput) {
     newItem.appendChild(newDesc)
     newDesc.appendChild(textContent)
     newDesc.classList.add("newDesc")
-    
+    undoButton.setAttribute('disabled', '')
+    currentState.push(newItem)
+    setHistory(currentState)
 }  
     
     
@@ -60,9 +94,13 @@ function createEdit(newItem, newDesc) {
         editItem(newItem, newDesc)
     })
 
+    newItem.addEventListener('dblclick', ()=> {
+        editItem(newItem, newDesc)
+    })
+
 }
 
-function createDone(newItem) {
+function createDone(newItem, newDesc) {
     const newButton = document.createElement("button")
     let textContent = document.createTextNode("Mark Done")
     newItem.appendChild(newButton)
@@ -72,47 +110,88 @@ function createDone(newItem) {
     newButton.addEventListener('click', ()=> {
         markDone(newItem)
     })
+
+    newDesc.addEventListener('click', ()=> {
+        markDone(newItem)
+    })
 }
 
 function deleteItem(parentDiv) {
+    let copyState = []
+    currentState.filter(element => {
+        if (element != parentDiv) {
+            copyState.push(element)
+        }
+
+        currentState = copyState
+    })
+    undoButton.removeAttribute('disabled')
+
     container.removeChild(parentDiv)
+    setHistory(currentState)
+    
 }
 
 function editItem(parentDiv, parentDesc) {
     const newInput = document.createElement("input")
     const newSubmit = document.createElement("button")
+
+    if (parentDiv.hasAttribute('edit') != true) {
+        let textContent = document.createTextNode("Submit")
+        parentDiv.appendChild(newSubmit)
+        newSubmit.classList.add("newSubmit")
+        newSubmit.appendChild(textContent)
     
-    let textContent = parentDesc.innerText
-    parentDiv.appendChild(newInput)
-    newInput.value = textContent
-    newInput.classList.add("newThings")
+        textContent = parentDesc.innerText
+        parentDiv.appendChild(newInput)
+        newInput.value = textContent
+        newInput.classList.add("newInput")
+        parentDiv.setAttribute('edit', '')
+    }
     
-    textContent = document.createTextNode("Submit")
-    parentDiv.appendChild(newSubmit)
-    newSubmit.classList.add("newThings")
-    newSubmit.appendChild(textContent)
-    
-    parentDesc.innerText = ''
-    parentDiv.removeChild(parentDesc)
+    parentDesc.setAttribute('hide', '')
     
     newSubmit.addEventListener('click', ()=> {
-        createItem(parentDiv, parentDesc, newInput)
         parentDiv.removeChild(newInput)
         parentDiv.removeChild(newSubmit)
         parentDiv.removeAttribute('done')
+        parentDiv.removeAttribute('edit')
+
+        parentDesc.innerText = newInput.value
+        parentDesc.removeAttribute('hide')
 
     })
 }
+
 
 function markDone(newItem) {
-    newItem.setAttribute("done", "")
+    if (newItem.hasAttribute('done') != true) {
+        newItem.setAttribute("done", "")
+    } else if (newItem.hasAttribute('done') == true) {
+        newItem.removeAttribute('done')
+    }
 
 }
 
-deleteButton.addEventListener('click', ()=> {
-    let doneItems = document.querySelectorAll('[done]')
-    doneItems.forEach(item => {
-        container.removeChild(item)
-    })
+
+function setHistory(state) {
+    switch(state) {
+        case currentState:
+            if (indexCount < 0) {
+                indexCount += 1
+                historyState.push(state.slice())
+            } else if (indexCount >= 0) {
+                indexCount += 1
+                historyState.push(state.slice())
+                console.log(indexCount)
+            }
+        break;
+
+        case true:
+            historyState.push(currentState.slice())
+            console.log(indexCount)
+        break;
+    }
+
     
-})
+}
