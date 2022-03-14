681a9505734be207598eeb018ccd4dcccc72a217
DESKTOP-3FG302D\darkb <Johnny Rangel>
2022-03-14 00:26:50 -0700

    Updating Drawing

--- data_web/My_Projects/Project_Pixel/resources/logic.js
+++ data_web/My_Projects/Project_Pixel/resources/logic.js
@@ -1,34 +1,39 @@
 const canvas = document.querySelector('canvas')
 const reset = document.querySelector('[reset]')
 
-canvas.style.setProperty('border', 'black solid 6px')
-canvas.style.setProperty('width', '40vw')
-canvas.style.setProperty('height', '50vh')
-
 // Figure out how to make it only track the mouse when the mouse is held down
+canvas.addEventListener('mousedown', ()=>{
+   doAction = true
+})
+
+canvas.addEventListener('mouseup', ()=>{
+    doAction = false
+ })
+
 canvas.addEventListener('mousemove', (e)=> {
-    x = e.clientX
-    y = e.clientY   
-    
-    console.log(x, y)
-}) 
+    if (doAction == true) {
+        let x = e.offsetX
+        let y = e.offsetY 
+        draw(x, y)
+    }
+})
+
 
 reset.addEventListener('click', ()=> {
     empty()
 })
 
-function draw(x,y) {
-    const line = canvas.getContext('2d')
-
-    line.beginPath()
-    line.moveTo(x, y);
-    line.lineTo(x, y);
-    line.stroke();
+function draw(x, y) {
+    const ctx = canvas.getContext('2d')
+    ctx.beginPath()
+    ctx.arc(x, y, 5, 0, 2 * Math.PI)
+    ctx.fill()
+    
 }
 
 function empty(){
-    const x = canvas.getContext('2d')
-    x.clearRect(0,0,canvas.width, canvas.height)
+    const ctx = canvas.getContext('2d')
+    ctx.clearRect(0,0,canvas.width, canvas.height)
 }
 
 
