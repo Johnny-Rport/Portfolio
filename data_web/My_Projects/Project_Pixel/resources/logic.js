86694fcfaf9b10874e0b2597de7f2e1846ce5a9f
DESKTOP-3FG302D\darkb <Johnny Rangel>
2022-03-17 19:51:42 -0700

    Updating Draw

--- data_web/My_Projects/Project_Pixel/resources/logic.js
+++ data_web/My_Projects/Project_Pixel/resources/logic.js
@@ -1,31 +1,79 @@
+class colorPicker {
+    constructor(color) {
+        this.color = color
+    }
+
+    setColor() {
+        switch (this.color) {
+            case 'red':
+                ctx.strokeStyle = '#FF0000'
+            break;
+
+            case 'blue':
+                ctx.strokeStyle = '#0000ff'
+            break;
+
+            case 'green':
+                ctx.strokeStyle = '#008000'
+            break;
+
+            case 'orange':
+                ctx.strokeStyle = '#ffa500'
+            break;
+
+            case 'purple':
+                ctx.strokeStyle = '#800080'
+            break;
+
+            case 'black':
+                ctx.strokeStyle = '#000000'
+            break;
+
+            default:
+                console.log("Broke")
+            break;
+        }
+    }
+    
+}
+
 const canvas = document.querySelector('canvas')
 const reset = document.querySelector('[reset]')
 const ctx = canvas.getContext('2d')
-let startAction = true
-let doAction = false
+const colors = document.querySelector('[color]')
+let color = []
+// Make an input for stroke width
+
+let doAct = false
 let x1 = 0
 let y1 = 0
 let x2
 let y2
 
 canvas.addEventListener('mousedown', ()=>{
-    doAction = true
-    startAction = false
+    doAct = true
 })
 
 canvas.addEventListener('mouseup', ()=>{
-    doAction = false
-    startAction = true
+    doAct = false
+    x2 = undefined
+    y2 = undefined
+})
+
+canvas.addEventListener('mouseout', ()=>{
+    doAct = false
+    x2 = undefined
+    y2 = undefined
  })
 
 canvas.addEventListener('mousemove', (e)=> {
-    if (doAction == true) {
+    if (doAct == true) {
         x1 = x2
         y1 = y2
         x2 = e.offsetX
         y2 = e.offsetY
-        
         draw()
+
     }
 })
 
@@ -34,20 +82,13 @@ reset.addEventListener('click', ()=> {
     empty()
 })
 
-// The draw function connects lines even after the mouse moves to a new position, try to use a startaction boolean
 function draw() {
-    if (startAction == true) {
-        ctx.beginPath()
-        ctx.arc(x1, y2, 5, 0, Math.PI * 2)
-        ctx.fill()
-    } else if (startAction != true) {
-        ctx.beginPath()
-        ctx.lineJoin = 'round'
-        ctx.moveTo(x1, y1)
-        ctx.lineTo(x2, y2)
-        ctx.stroke()
-    }
-    
+    ctx.beginPath()
+    ctx.lineJoin = 'round'
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.stroke()
+
     requestAnimationFrame(draw)
 }
 
@@ -55,4 +96,13 @@ function empty(){
     ctx.clearRect(0,0,canvas.width, canvas.height)
 }
 
+for (let item = 0; item < colors.childElementCount; item++) {
+    color.push(colors.children[item])   
+}
 
+color.forEach(item => {
+    item.addEventListener('click', ()=> {
+        let brush  = new colorPicker(item.attributes[0].name)
+        brush.setColor()
+    })
+})
